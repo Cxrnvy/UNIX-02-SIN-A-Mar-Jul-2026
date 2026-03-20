90f54f76ba2fae080d7fb6f4d41c2d695b54269e
Cexnvy <cearciniegasme@uide.edu.ec>
2026-03-20 18:08:00 +0000

    key lists

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,6 @@ uname -a #Daba informacion del ordenador (arquitectura, SO)
 which gpg #Brinda la ubicacion del binario del GPG
 gpg --version #Da la versión e info adicional
 gpg --full-generate-key #Permite generar la llave publica y privada
+gpg --list-keys #Enlista las llaves
+gpg --armor --export cesar.arciniegas.mejia4@gmail.com> mi_llave_publica.asc #Exporta las llaves
+#EL ">" redirige la salida extanar a una salida de texto
