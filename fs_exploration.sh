b0480c6f64a94ef69b8d2f1ae13e7106353842ac
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-02 13:17:16 +0000

    wip: UEFI or BIOS

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -15,6 +15,10 @@ sudo apt install parted
 
 #It examines and displays in an orderly manner on the screen the structure, formats, and partitions of all disks connected to the computer.
 sudo parted -l && echo -e "\n--\n" && lsblk -f && echo -e "\n--\n"
-&& #This means that the command on the right will only be executed if the command on the left finished successfully (without errors).
+&& #This means that the command on the right will only be executed if the command on the left finished successfully (without errors). (AND)
 #msdos = mbr
+
+#This command is used to quickly determine if the system booted in UEFI mode or traditional BIOS mode.
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
+|| #(OR) They mean "or else" (it's the opposite of &&). If the initial check fails (the folder doesn't exist), it skips the previous step and prints "BIOS".
 
