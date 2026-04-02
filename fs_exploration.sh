103029fe720d1afe47b821b7b39042151b6a88a6
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-02 12:46:14 +0000

    code: Directory files

---
+++ fs_exploration.sh
@@ -0,0 +1,5 @@
+#This command changes to the root directory where the other directories and files are located, which operate as a hierarchical system identified with a "/"
+cd /
+
+#It lists the contents of a directory, but adds a special character to indicate the file type.
+ls -F
