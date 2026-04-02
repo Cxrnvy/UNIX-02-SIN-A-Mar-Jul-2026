00db3d185e8f3126eb90e13fbf95b46af15332ab
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-02 12:56:13 +0000

    wip: Install parted

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -3,3 +3,13 @@ cd /
 
 #It lists the contents of a directory, but adds a special character to indicate the file type.
 ls -F
+
+#This command synchronizes the local software list with the official repositories. It allows the system to know the latest available versions without installing anything.
+sudo apt update
+
+#This instruction is responsible for downloading and installing the latest versions of the programs.
+sudo apt upgrade
+
+#This command specifically downloads and installs the parted program on the system. This tool is essential for the user to manage, create, and resize disk partitions.
+sudo apt install parted
+
