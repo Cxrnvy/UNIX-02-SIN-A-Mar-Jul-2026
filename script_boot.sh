680b452aa02ff09363c55bfcf9071b3d651d15a8
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-09 12:43:41 +0000

    chore: add commands to check bash and paths

--- script_boot.sh
+++ script_boot.sh
@@ -2,3 +2,12 @@ git branch #Command for checking in which branch is the codespace
 mkdir #Create a new directory/folder
 cd proyecto/ #Relative Rute, change to the folder (relative path)
 cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #Absolute Rute, moves to the exact folder
+
+
+cd #Moves to home directory
+cd ~ #Moves to home directory
+cd /home/codespace #Moves to the codespace folder (Absolute Rute)
+cd $HOME #Moves to the home directory 
+echo $HOME #Shows your home directory path.
+echo $BASH #Shows where Bash is installed.
+echo $PATH #Shows folders where the system looks for commands.
