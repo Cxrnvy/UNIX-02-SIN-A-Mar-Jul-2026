00b5fe6f18a2bff237b422cd07b9abcf27437c01
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-09 12:32:28 +0000

    feat: Directory  and rutes

---
+++ script_boot.sh
@@ -0,0 +1,4 @@
+git branch #Command for checking in which branch is the codespace  
+mkdir #Create a new directory/folder
+cd proyecto/ #Relative Rute, change to the folder (relative path)
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto #Absolute Rute, moves to the exact folder
