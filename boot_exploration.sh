816aa88276ebd7dd081269d90c5530cc5d5359cf
Cexnvy <cearciniegasme@uide.edu.ec>
2026-04-16 12:56:42 +0000

    wip: list

---
+++ boot_exploration.sh
@@ -0,0 +1,6 @@
+#-l (long format): Shows permissions, owner/group, size, modification date/time, and name. -a (all): Includes hidden files (those starting with ., like .bashrc). -h (human-readable): File sizes in KB, MB, GB (e.g., 1.2M) instead of raw bytes
+ls -l -a -h
+#This command is exactly equivalent to the previous one
+#-l -a -h (separated) = -lah (combined).
+ls -lah
+
