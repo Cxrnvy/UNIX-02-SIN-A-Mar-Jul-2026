08e3b22eb124b7190081b7ea47cb7b2b5282b25e
Cexnvy <cearciniegasme@uide.edu.ec>
2026-05-12 16:58:11 +0000

    wip: umask

---
+++ security.zsh
@@ -0,0 +1,29 @@
+    1  umask
+    2  touch archivo1
+    3  mkdir directorio1
+    4  ls -l
+    5  umask 027
+    6  touch archivo2
+    7  mkdir diretorio2
+    8  ls -l
+    9  umask
+   10  umask 027
+   11  ls -l
+   12  sudo apt-get update
+   13  apt-get update
+   14  apt-get upgrade
+   15  apt-get install acl
+   16  chown -R $(whoami)
+   17  sudo chown -R $(whoami)
+   18  chown -R $(whoami) .
+   19  setfatcl -bnR .
+   20  setfacl -bnR .
+   21  ls- l
+   22  ls -l
+   23  umask 027
+   24  ls -l
+   25  whoami
+   26  umask
+   27  touch archivo3
+   28  mkdir directorio3
+   29  ls -l
