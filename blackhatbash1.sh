0a9e879d35b3ba49910383f457e1515ba3324521
Cexnvy <cearciniegasme@uide.edu.ec>
2026-06-04 12:55:18 +0000

    wip: shebang practice

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,13 @@
+#!/bin/bash -x
 bash --version
 env
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE} 
+#man ls
+ps -ef
+df --human-readable
+#bash -r blackhatbash1.sh
+#bash -n blackhatbash1.sh
+#bash -x blackhatbash1.sh
