b9e1bc01d82d3d6c7af7a0902005b06a118c33d9
Cexnvy <cearciniegasme@uide.edu.ec>
2026-06-18 13:09:47 +0000

    Combine grep and awk

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -16,3 +16,11 @@ awk '{print $3}' log.txt
 awk '{print $1,$NF}' log.txt
 awk -F',' '{print $1}' example_csv.txt
 
+head log.txt
+awk 'NR < 10' log.txt
+#1.
+grep "42.236.10.117" log.txt 
+#2. 
+awk '{print $7}'
+
+$ grep "42.236.10.117" log.txt | awk '{print $7}'
