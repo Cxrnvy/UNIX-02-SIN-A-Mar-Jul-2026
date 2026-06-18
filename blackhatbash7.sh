42a584935edf08011e28f39c13c00fbb4c009798
Cexnvy <cearciniegasme@uide.edu.ec>
2026-06-18 13:04:30 +0000

    coma separated file

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -14,3 +14,5 @@ awk '{print $2}' log.txt
 awk '{print $3}' log.txt
 
 awk '{print $1,$NF}' log.txt
+awk -F',' '{print $1}' example_csv.txt
+
---
+++ tester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for i in {1..3}
+do
+    echo "Hola$i,Chao $i" >> example_csv.txt
+    done
+
+    
